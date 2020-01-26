f2696ad56bcc8ed6b72e9d3ee5de6f9be953ff77
Rbee <52832186+Abi-Rai@users.noreply.github.com>
2020-01-26 11:04:27 +0000

    changing index

---
+++ StoreLibrary/Migrations/20200126095339_checkChanges.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoreLibrary.Migrations
+{
+    public partial class checkChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Requests",
+                maxLength: 150,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Message",
+                table: "Requests",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 150,
+                oldNullable: true);
+        }
+    }
+}
